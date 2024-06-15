caf3ca479bfe25829940beb4040e3099f7698693
GaneshPalakula <56264186+GANESH7692@users.noreply.github.com>
2024-06-15 22:48:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require('express');
 const axios = require('axios');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+// Serve the index.html file directly
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -14,9 +15,11 @@ app.get('/get-video-url', async (req, res) => {
 
     try {
         const response = await axios.get(queryUrl);
+        console.log('API response:', response.data); // Log the response data
         const videoUrl = response.data.data.shortcode_media.video_url;
         res.json({ video_url: videoUrl });
     } catch (error) {
+        console.error('Error fetching video URL:', error.response ? error.response.data : error.message); // Log the error
         res.status(500).json({ error: 'Failed to fetch video URL' });
     }
 });
